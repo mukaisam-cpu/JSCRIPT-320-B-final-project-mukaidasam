c98b3ba0308a72134d0c294a85061cfbf6ca9d25
mukaisam-cpu <mukaisam@uw.edu>
2026-02-24 18:20:05 -0800

    Initial commit of vite project

---
+++ ra-viewer/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>Under construction.</p>
+      <p>How did you get here, anyway?</p>
+    </>
+  )
+}
+
+export default App
