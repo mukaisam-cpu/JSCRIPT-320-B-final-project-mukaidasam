f0f2f7ea7b714fde5c2cde6c88eb90905dcc01bd
mukaisam-cpu <mukaisam@uw.edu>
2026-03-02 17:21:35 -0800

    Implemented .env

--- ra-viewer/src/routes/Home.jsx
+++ ra-viewer/src/routes/Home.jsx
@@ -1,8 +1,43 @@
+import { useEffect, useState } from 'react'
+import { PacmanLoader } from 'react-spinners';
+
+const getSystemListURL = ""
+
 function Home() {
-    return (<>
+    const [_error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [gameCards, setGameCards] = useState(<></>);
+
+    // useEffect(() => {
+    //     console.log('useEffect Home');
+    //     fetch(getGameListURL)
+    //         .then(response => response.json())
+    //         .then(
+    //             data => {
+    //                 console.log(data);
+    //                 setLoading(false);
+    //                 setGameCards(data.map((game, index) =>
+    //                     <GameCard
+    //                         index={index}
+    //                         id={game.ID}
+    //                         title={game.Title}
+    //                         image={game.ImageIcon}
+    //                     />
+    //                 ));
+    //             },
+    //             error => {
+    //                 console.log(error)
+    //                 setError(true);
+    //                 setLoading(false);
+    //             }
+    //         )
+    // }, []) // Rate limiting array
+
+    return (<div>
         <p>This is the home page. Under construction.</p>
         <p>How did you get here, anyway?</p>
-    </>)
+        <p>Psst, the key is {import.meta.env.VITE_RA_KEY}.</p>
+    </div>)
 }
 
 export default Home
